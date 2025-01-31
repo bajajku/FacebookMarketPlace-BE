8a23cf66c1dac6eeeb4c18b02b3b257595b07dfd
bajajku <bajajku@sheridancollege.ca>
2025-01-31 10:04:32 -0500

    Update Program.cs with standard ASP.NET Core configuration and Swagger setup

--- FacebookMarketPlace/Program.cs
+++ FacebookMarketPlace/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Facebook Marketplace API v1");
+        c.RoutePrefix = "swagger";
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
